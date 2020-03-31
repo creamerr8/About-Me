07a8d6c288f28d8113b16c22a33876a8767e1d6e
creamerr8 <creamerr8@gmail.com>
2020-03-31 13:43:55 -0700

    added new files for webpage, created 5 question prompts with alerts, added html, and styled html

---
+++ app.js
@@ -0,0 +1,44 @@
+// 'use strict'
+
+
+// // This is for my welcome message
+// var welcomeName = prompt('Welcome to my website! My name is Ryan what is your name?');
+// var WelcomeMsg = alert('Hi ' + welcomeName + '! Glad to see you here! I have a couple questions for you!')
+
+
+// // These are 5 yes or no questions using if statements
+// var q1 = prompt('Do you like soccer?');
+//   if (q1 === 'yes'){
+// alert('Hey me to!');
+//   } else {
+//     alert('dang, its such a good sport');
+//   }
+
+//   var q2 = prompt('Do you like video games?');
+//   if (q2 === 'yes'){
+// alert('I love videogames!');
+//   } else {
+//     alert('Thats ok, a lot of people dont');
+//   }
+  
+
+//   var q3 = prompt('Do you want to become a programmer?');
+//   if (q3 === 'yes'){
+// alert('Me too! I really want to make video games!');
+//   } else {
+//     alert('Well not everybody wants to be a programmer, but I think its super cool!');
+//   }
+
+//   var q4 = prompt('Are you from Washington?');
+//   if (q4 === 'yes'){
+// alert('Thats rad! I was born here and have lived here all my life!');
+//   } else {
+//     alert('Ok, I wonder where you are from?');
+//   }
+
+//   var q5 = prompt('Have you ever left the country?');
+//   if (q5 === 'yes'){
+// alert('Nice! I have only been to Canada');
+//   } else {
+//     alert('You should! There are so many cool places to go! I have a few in mind I would love to see!');
+//   }
