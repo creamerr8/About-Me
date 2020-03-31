889e9d59cf77def645166c011bf430091025cb05
creamerr8 <creamerr8@gmail.com>
2020-03-31 16:47:34 -0700

    fixed bug

--- app.js
+++ app.js
@@ -7,37 +7,37 @@ var WelcomeMsg = alert('Hi ' + welcomeName + '! Glad to see you here! I have a c
 
 
 // These are 5 yes or no questions using if statements
-var q1 = prompt('Do you like soccer?');
-  if (q1 === 'yes'.toLocaleLowerCase){
+var q1 = prompt('Do you like soccer?').toLocaleLowerCase();
+  if (q1 === 'yes'){
 alert('Hey me to!');
   } else {
     alert('dang, its such a good sport');
   }
 
-  var q2 = prompt('Do you like video games?');
-  if (q2 === 'yes'.toLocaleLowerCase){
+  var q2 = prompt('Do you like video games?').toLocaleLowerCase();
+  if (q2 === 'yes'){
 alert('I love videogames!');
   } else {
     alert('Thats ok, a lot of people dont');
   }
   
 
-  var q3 = prompt('Do you want to become a programmer?');
-  if (q3 === 'yes'.toLocaleLowerCase){
+  var q3 = prompt('Do you want to become a programmer?').toLocaleLowerCase();
+  if (q3 === 'yes') {
 alert('Me too! I really want to make video games!');
   } else {
     alert('Well not everybody wants to be a programmer, but I think its super cool!');
   }
 
-  var q4 = prompt('Are you from Washington?');
-  if (q4 === 'yes'.toLocaleLowerCase){
+  var q4 = prompt('Are you from Washington?').toLocaleLowerCase();
+  if (q4 === 'yes') {
 alert('Thats rad! I was born here and have lived here all my life!');
   } else {
     alert('Ok, I wonder where you are from?');
   }
 
-  var q5 = prompt('Have you ever left the country?');
-  if (q5 === 'yes'.toLocaleLowerCase){
+  var q5 = prompt('Have you ever left the country?').toLocaleLowerCase();
+  if (q5 === 'yes') {
 alert('Nice! I have only been to Canada');
   } else {
     alert('You should! There are so many cool places to go! I have a few in mind I would love to see!');
